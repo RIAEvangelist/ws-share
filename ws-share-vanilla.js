5bf42c56e390514ea73f6de449b290e0063eb5a5
Brandon Nozaki Miller <brandon@diginow.it>
2016-02-16 10:41:43 -0800

    incorporated latest changes into vanilla version

--- ws-share-vanilla.js
+++ ws-share-vanilla.js
@@ -1,98 +1,94 @@
 (
     function(){
-        var wsList={};
+        var WebSocket=null;
+
+        if(isNode()){
+            WebSocket = require('ws');
+        }else{
+            WebSocket=window.WebSocket;
+        }
+
+        const wsList={};
 
         function wsClosed(e){
             delete wsList[this._WS_KEY];
         }
 
-        function WS(uri,protocols){
-            if(!uri){
-                throw('WS requires a uri to initialize');
-            }
-            if(!wsList[uri+protocols]){
-                var newWS=null;
-                if(protocols){
-                    newWS=new WebSocket(uri,protocols);
-                }else{
-                    newWS=new WebSocket(uri);
+        class WS{
+            constructor(uri,protocols){
+                if(!uri){
+                    throw('WS requires a uri to initialize');
                 }
+                let newWS=null;
+                if(!wsList[uri+protocols]){
 
-                newWS._WS_KEY=uri+protocols;
+                    if(protocols){
+                        newWS=new WebSocket(uri,protocols);
+                    }else{
 
-                newWS.addEventListener(
-                    'close',
-                    wsClosed
-                );
-
-                wsList[uri+protocols]=newWS;
-            }
-            var ws=wsList[uri+protocols];
-
-            Object.defineProperties(
-                this,
-                {
-                    uri:{
-                        writable:false,
-                        enumerable:true,
-                        value:uri
-                    },
-                    protocols:{
-                        writable:false,
-                        enumerable:true,
-                        value:protocols
-                    },
-                    on:{
-                        writable:false,
-                        enumerable:true,
-                        value:ws.addEventListener.bind(ws)
-                    },
-                    off:{
-                        writable:false,
-                        enumerable:true,
-                        value:ws.removeEventListener.bind(ws)
-                    },
-                    addEventListener:{
-                        writable:false,
-                        enumerable:true,
-                        value:ws.addEventListener.bind(ws)
-                    },
-                    removeEventListener:{
-                        writable:false,
-                        enumerable:true,
-                        value:ws.removeEventListener.bind(ws)
-                    },
-                    send:{
-                        writable:false,
-                        enumerable:true,
-                        value:ws.send.bind(ws)
-                    },
-                    readyState:{
-                        get:getReadyState,
-                        set:getReadyState
-                    },
-                    toObject:{
-                        get:getAsObject,
-                        set:getAsObject
+                        newWS=new WebSocket(uri);
                     }
+
+                    newWS._WS_KEY=uri+protocols;
+                    wsList[uri+protocols]=newWS;
                 }
-            );
+                const ws=wsList[uri+protocols];
 
-            function getReadyState(){
-                return ws.readyState;
-            }
+                if(isNode()){
+                    ws.addEventListener = ws.addListener;
+                    ws.removeEventListener = ws.removeListener;
+                }else{
+                    ws.addListener = ws.addEventListener;
+                    ws.removeListener = ws.removeEventListener;
+                }
 
-            function getAsObject(){
-                return Object.assign(
-                    {},
-                    this
+                if(newWS){
+                    ws.addEventListener(
+                        'close',
+                        wsClosed
+                    );
+                }else{
+                    return ws;
+                }
+
+                Object.defineProperties(
+                    ws,
+                    {
+                        uri:{
+                            writable:false,
+                            enumerable:true,
+                            value:uri
+                        },
+                        protocols:{
+                            writable:false,
+                            enumerable:true,
+                            value:protocols
+                        },
+                        on:{
+                            writable:false,
+                            enumerable:true,
+                            value:ws.addEventListener
+                        },
+                        off:{
+                            writable:false,
+                            enumerable:true,
+                            value:ws.removeEventListener
+                        }
+                    }
                 );
-            }
 
-            function getReadyState(){
-                return ws.readyState;
+                return ws;
             }
+        }
 
+        function isNode() {
+            let isNode = true;
+            try {
+                isNode = ( Window ) ? false : true;
+            } catch ( err ) {
+                //not the browser
+            }
+            return isNode;
         }
 
         window.WS=WS;
